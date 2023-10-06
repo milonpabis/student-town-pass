d78aa18e22dc703d5e35cd4783a6e0566a31b160
milonpabis <miloszpabis@onet.pl>
2023-10-06 19:44:04 +0200

    Deleted unnecessary images

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from faker import Faker
 from gui import Window, QApplication
 
-im = r"F:\Desktop\repos\sandbox\PIL\portrety\klaudia_karyna_drogoń.jpg"
 
 
 if __name__ == "__main__":
@@ -10,6 +9,4 @@ if __name__ == "__main__":
     window.show()
     app.exec()
 
-    # Faker(year="2003", month="1", day="4", name="MILOSZ", second_name="BORYS", surname="PABIS", photo=im,
-    # album="417356", show_day="28", show_month="9", pesel="04230909696",
-    # directory=r"F:\Desktop\kurwa.jpg", random=False)
+
